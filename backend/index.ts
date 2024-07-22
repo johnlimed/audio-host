16cf592e0fdb513d5f26fd69bacf47ab1068bb1c
John Lim <john.lim@aisoftwaredevelop.com>
2024-07-22 18:59:54 +0800

    feat(shutdown): Added shutdown middleware for graceful shutdown

--- backend/index.ts
+++ backend/index.ts
@@ -1,29 +1,28 @@
 import Koa from "koa";
+import http from 'http';
 import router from "./router";
 import { initDB } from "./database";
 import { logger } from "./logger";
 import { loggerMiddleware } from "./middleware/loggerMiddleware";
 import { errorMiddleware } from "./middleware/errorMiddleware";
 import { dbMiddleware } from "./middleware/dbMiddleware";
+import { shutdown } from "./middleware/shutdown";
 
 const startServer = () => {
   const app = new Koa();
   const port = 3000;
   const db = initDB(logger);
-
+  const server = http.createServer(app.callback());
+  
   app.use(loggerMiddleware(logger));
   app.use(dbMiddleware(db));
+  app.use(shutdown(logger, db, server));
   app.on("error", errorMiddleware);
   app.use(router.routes());
   app.use(router.allowedMethods());
 
-  app.listen(port, () => {
+  server.listen(port, () => {
     logger.info(`🚀 Server is running on port http://localhost:${port}/`);
-  });
-
-  process.on('SIGINT', function () {
-    logger.info("[dbMiddleware] flushing database");
-    db.close();
   });
 }
 
---
+++ backend/middleware/shutdown.ts
@@ -0,0 +1,34 @@
+import http from 'http';
+import { Logger } from "winston";
+
+export const shutdown = (logger: Logger, db: Loki, server: http.Server) => {
+  let shutdown = false;
+
+  const exit = () => {
+    if (shutdown) {
+      return;
+    }
+    shutdown = true;
+    logger.info("[shutdown] Server shutting down...");
+    logger.info("[shutdown] flushing database");
+    db.close();
+
+    server.close(() => {
+      logger.info('[shutdown] Closed out remaining connections');
+      process.exit(0);
+    });
+  }
+
+  process.on('SIGINT', () => exit());
+  process.on("SIGTERM", () => exit());
+
+  return (ctx, next) => {
+    if (shutdown) {
+      ctx.status = 503;
+      ctx.set('Connection', 'close');
+      ctx.body = 'Server is in the process of shutting down';
+    } else {
+      return next();
+    }
+  };
+}
